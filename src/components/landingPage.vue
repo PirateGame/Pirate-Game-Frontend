4f7316b2fe9ccbb4fceb134b5231a372296e34fb
Thomas Wood <33393308+ThomasAWood@users.noreply.github.com>
2020-12-18 16:11:55 +0000

    Added Navigation to the landing page

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,23 +1,14 @@
 <template>
     <div class="bg-pirate">
-        <!--<div class="accountBtns">
-            <button class="bg-red">Login</button>
-            <button class="bg-red">Register</button>
-        </div>-->
-        <h2 class="title">The Pirate Game</H2>
+        <h2 class="title">The Pirate Game</h2>
         <div class="mainMenu">
             <ul id="menuUL">
                 <li class="bg-blue">
-                    <a>Host Game</a>
+                    <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
                 </li>
-                <li class="bg-green" onclick="ShowPopUp()">
-                    <router-link to="">Join Game</router-link>
+                <li class="bg-green">
+                    <router-link :to="{ name: 'JoinPage' }">Join Game</router-link>
                 </li>
-                <!--<li class="bg-red">
-                    <a>
-                        Exit
-                    </a>
-                </li>-->
                 <li id="inputLI">
                 </li>
             </ul>
@@ -27,6 +18,6 @@
 
 <script>
 export default {
-    name: 'landingPage'
+    name: 'LandingPage'
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import LandingPage from '../components/LandingPage.vue'
 import HostPage from '../components/HostPage.vue'
-import ViewPage from '../components/ViewPage.vue'
+import JoinPage from '../components/JoinPage.vue'
 
 let router = createRouter({
     history: createWebHistory(),
     routes: [
-      { path: '/', component: LandingPage },
-      { path: '/host', component: HostPage },
-      { path: '/view', component: ViewPage }
+      { path: '/', component: LandingPage, name: 'LandingPage' },
+      { path: '/host', component: HostPage, name: 'HostPage' },
+      { path: '/join', component: JoinPage, name: 'JoinPage' }
 
     ]
   });
