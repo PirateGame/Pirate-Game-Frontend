586cdbe31183927b05e02b511a91b0284074b9ac
Alex40144 <giddyraccoon@gmail.com>
2020-12-18 19:08:03 +0000

    Revert "font change"
    This reverts commit 22cfc9aa122944ff1b65ddb586f9fa7e525b9397.

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< Updated upstream
     <div class="bg-pirate">
         <div class="config-box">
             <h1 class="title">The Pirate Game</h1>
@@ -11,21 +10,6 @@
                     <input type="button" value="Join" style="color: white; text-decoration: none;" class="big-button bg-green">
                 </div>
             </form>
-=======
-    <div class="bg-pirate body">
-        <h1 class="big-title">The Pirate Game</h1>
-        <div class="mainMenu">
-            <ul id="menuUL">
-                <li class="bg-blue">
-                    <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
-                </li>
-                <li class="bg-green">
-                    <router-link :to="{ name: 'JoinPage' }">Join Game</router-link>
-                </li>
-                <li id="inputLI">
-                </li>
-            </ul>
->>>>>>> Stashed changes
         </div>
     </div>
 </template>
