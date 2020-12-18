c982c05c218027719a696983886ece9e5646ac95
Thomas Wood <33393308+ThomasAWood@users.noreply.github.com>
2020-12-18 15:39:13 +0000

    Added the other pages to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import LandingPage from '../components/landingPage.vue'
-
+import LandingPage from '../components/LandingPage.vue'
+import HostPage from '../components/HostPage.vue'
+import ViewPage from '../components/ViewPage.vue'
 
 let router = createRouter({
     history: createWebHistory(),
     routes: [
-      { path: '/', component: LandingPage }
+      { path: '/', component: LandingPage },
+      { path: '/host', component: HostPage },
+      { path: '/view', component: ViewPage }
+
     ]
   });
 
