3795ce3d4d8361c1b798a458b784482f77948e0d
Alex40144 <giddyraccoon@gmail.com>
2020-12-19 11:48:32 +0000

    fixed landing screen buttons

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="bg-pirate">
-        <div class="config-box">
-            <h1 class="title">The Pirate Game</h1>
-            <form>
-                <div style="text-align: center;">
-                    <input type="button" value="Host" style="color: white; text-decoration: none;" class="big-button bg-blue">
-                </div>
-                <div style="text-align: center;">
-                    <input type="button" value="Join" style="color: white; text-decoration: none;" class="big-button bg-green">
-                </div>
-            </form>
+        <h1 class="title">The Pirate Game</h1>
+        <div class="mainMenu">
+            <ul id="menuUL">
+                <li class="bg-blue">
+                    <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
+                </li>
+                <li class="bg-green">
+                    <router-link :to="{ name: 'JoinPage' }">Join Game</router-link>
+                </li>
+                <li id="inputLI">
+                </li>
+            </ul>
         </div>
     </div>
 </template>
