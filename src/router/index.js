87aac5c13ebbeb9dda53a131e8fb7e48cb02f131
Alex40144 <giddyraccoon@gmail.com>
2020-12-22 13:46:30 +0000

    added admin panel page
    this is used to oversee all games in progress

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HostPage from '../components/HostPage.vue'
 import JoinPage from '../components/JoinPage.vue'
 import WaitingRoom from '../components/WaitingRoom.vue'
 import HostPanel from '../components/HostPanel.vue'
+import AdminPanel from '../components/AdminPanel.vue'
 
 
 let router = createRouter({
@@ -14,6 +15,7 @@ let router = createRouter({
       { path: '/host', component: HostPage, name: 'HostPage' },
       { path: '/join', component: JoinPage, name: 'JoinPage' },
       { path: '/HostPanel', component: HostPanel, name: 'HostPanel' },
+      { path: '/AdminPanel', component: AdminPanel, name: 'AdminPanel' },
       { path: '/WaitingRoom', component: WaitingRoom, name: 'WaitingRoom' }
 
     ]
