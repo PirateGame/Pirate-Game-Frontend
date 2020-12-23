db4b0e65d5ae886a645e0550b5e73689fde80ed7
Alex40144 <giddyraccoon@gmail.com>
2020-12-23 19:47:20 +0000

    grids working. YAY

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="bg-pirate">
-        <h2>Drag and drop the tiles to create your board, or hit the randomise button.</h2>
-          <div class="board-holder">
-            <div :class="['grid grid-stack grid-stack-' + gridWidth]"></div>
-          </div>
-          <div id="sidebar" :class="['bar grid-stack grid-stack-' + 1]"></div>
-        </div>
+  <div class="bg-pirate">
+  <h2>Drag and drop the tiles to create your board, or hit the randomise button.</h2>
+    <div class="board-holder">
+      <div :class="['grid-stack grid-stack-' + gridWidth]"></div>
+    </div>
+    <div class="board-holder-narrow">
+      <div :class="['grid-stack grid-stack-' + 1]"></div>
+    </div>
+  </div>
 </template>
 <script>
 import Axios from '/services/axios.js';
@@ -26,13 +28,30 @@ export default {
     },
     async mounted () {
         var items = [
-          {content: 'my first widget',noResize: true, noMove:false},
-          {content: 'another widget!',noResize: true, noMove:false}
+          {content: 'Kill',noResize: true, noMove:false},
+          {content: 'Steal',noResize: true, noMove:false},
+          {content: 'Swap',noResize: true, noMove:false}
+        ];
+
+        //this is pre placed to stop the grid from disappearing
+        var MANDATORYitems = [
+          {content: '£5000',noResize: true, noMove:false}
         ];
         
-        var grid = GridStack.init({class:"grid",float: true, column:this.gridWidth, minRow:this.gridHeight, maxRow:this.gridHeight});
-        var sidebar = GridStack.init({class:"bar",float: false, column:1});
-        grid.load(items);
+        let grids = GridStack.initAll({
+          dragIn: '.grid-stack-item',
+          dragInOptions: { revert: 'invalid', scroll: false, appendTo: 'body', helper: 'clone' },
+          acceptWidgets: function(el) { return true; }
+        });
+        grids[0].float(true);
+        grids[0].column(this.gridWidth);
+        grids[0].opts.minRow = this.gridHeight;
+        grids[0].opts.maxRow = this.gridHeight;
+        grids[1].float(false);
+        grids[1].column(1);
+        grids[1].opts.cellHeight = 40; //pixels
+        grids[0].load(MANDATORYitems);
+        grids[1].load(items);
     }
 }
 
