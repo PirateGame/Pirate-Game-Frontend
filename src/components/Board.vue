e074b8730339d12a42845607b9cf8d749b856afd
Alex40144 <giddyraccoon@gmail.com>
2020-12-23 08:47:53 +0000

    gridstack imported

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,13 +3,20 @@
         <h1 class="title">Please wait</h1>
         <h2>Game Board</h2>
         <h2>your secret code is: {{secretcode}}</h2>
+        <div class="grid-stack"></div>
     </div>
 </template>
 <script>
 import Axios from '/services/axios.js';
 import router from '../router/index';
+import 'gridstack/dist/gridstack.min.css';
+import GridStack from 'gridstack';
 export default {
     name: 'WaitingRoom',
+    async mounted () {
+        var grid = GridStack.init();
+        grid.addWidget({w: 2, content: 'item 1'});
+    }
 }
 
 </script>
