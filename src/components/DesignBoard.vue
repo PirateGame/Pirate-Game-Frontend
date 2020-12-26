c2fae877c8eda01e856a2b4b0140a5e2e07d8d9c
Alex40144 <giddyraccoon@gmail.com>
2020-12-26 21:30:12 +0000

    save and load grid on designer

--- src/components/DesignBoard.vue
+++ src/components/DesignBoard.vue
@@ -5,7 +5,7 @@
       <div :class="['grid-stack grid-stack-' + gridWidth]"></div>
     </div>
     <div class="board-holder-narrow">
-      <input type="button" value="Randomize" style="color: white; text-decoration: none;" class="big-button bg-red" @click="RandomizeBoard">
+      <input type="button" value="Randomise" style="color: white; text-decoration: none;" class="big-button bg-red" @click="randomiseBoard">
       <div :class="['grid-stack grid-stack-' + 1]"></div>
       <input type="button" value="Submit" style="color: white; text-decoration: none;" class="big-button bg-blue" @click="submitBoard">
     </div>
@@ -32,14 +32,12 @@ export default {
         }
     },
     async mounted () {
-        var items = [
-          {content: 'Kill',noResize: true, noMove:false},
-          {content: 'Steal',noResize: true, noMove:false}
-        ];
+        //get all the tiles that should be placed in the grid
         let response = null;
-        response = await Axios().post('getNumTiles',
+        response = await Axios().post('getBarTiles',
             {
                 gameName: this.gameName,
+                playerName: this.playerName
             }
         );
         if (response.data["game"] == false){
@@ -48,43 +46,58 @@ export default {
         }
 
         console.log(response.data)
-        items = response.data;
+        var items = response.data;
 
         //this is pre placed to stop the grid from disappearing
         var MANDATORYitems = [
           {content: '£5000',noResize: true, noMove:false}
         ];
         
-        let grids = GridStack.initAll({
+
+        //load grids
+        this.grids = GridStack.initAll({
           dragIn: '.grid-stack-item',
           dragInOptions: { revert: 'invalid', scroll: false, appendTo: 'body', helper: 'clone' },
           acceptWidgets: function(el) { return true; }
         });
-        grids[0].float(true);
-        grids[0].column(this.gridWidth);
-        grids[0].opts.minRow = this.gridHeight;
-        grids[0].opts.maxRow = this.gridHeight;
-        grids[1].float(false);
-        grids[1].column(1);
-        grids[1].opts.cellHeight = 40; //pixels
-        grids[0].load(MANDATORYitems);
-        grids[1].load(items);
+        this.grids[0].float(true);
+        this.grids[0].column(this.gridWidth);
+        this.grids[0].opts.minRow = this.gridHeight;
+        this.grids[0].opts.maxRow = this.gridHeight;
+        this.grids[1].float(false);
+        this.grids[1].column(1);
+        this.grids[1].opts.cellHeight = 40; //pixels
+        this.grids[0].load(MANDATORYitems);
+        this.grids[1].load(items);
     },
     methods: {
       async submitBoard(){
         serializedData = grid.save();
         let response = null;
-          response = await Axios().post('getPlayers',
+        response = await Axios().post('saveBoard',
+            {
+                gameName: this.gameName,
+                playerName: this.playerName,
+                authCode: this.authcode,
+                board: serializedData
+            });
+        if (response.data["game"] == false){
+            alert("game not found")
+        }
+
+      },
+      async randomiseBoard (){
+        this.grids[1].removeAll();
+        let response = null;
+        response = await Axios().post('randomiseBoard',
               {
                   gameName: this.gameName,
                   playerName: this.playerName,
-                  authCode: this.authcode,
-                  board: serializedData
+                  authCode: this.authcode
               });
-          if (response.data["game"] == false){
-              alert("game not found")
-          }
-
+        var board = response.data;
+        console.log(board);
+        this.grids[0].load(board, true);
       }
     }
 }
--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -9,9 +9,6 @@
                 <li class="bg-blue">
                     <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
                 </li>
-                <li class="bg-red">
-                    <router-link :to="{ name: 'DesignBoard' }">Game Board Test</router-link>
-                </li>
                 <li id="inputLI">
                 </li>
             </ul>
