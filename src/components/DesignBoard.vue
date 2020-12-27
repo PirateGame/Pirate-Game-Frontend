8d5b671b47d6d71a82dac4034d7f1e99f9ab62b2
Alex40144 <giddyraccoon@gmail.com>
2020-12-27 10:26:49 +0000

    redone grids.

--- src/components/DesignBoard.vue
+++ src/components/DesignBoard.vue
@@ -2,11 +2,11 @@
   <div class="bg-pirate">
   <h2>Drag and drop the tiles to create your board, or hit the randomise button.</h2>
     <div class="board-holder">
-      <div :class="['grid-stack grid-stack-' + gridWidth]"></div>
+      <div :class="['grid-stack board grid-stack-' + gridWidth]"></div>
     </div>
     <div class="board-holder-narrow">
       <input type="button" value="Randomise" style="color: white; text-decoration: none;" class="big-button bg-red" @click="randomiseBoard">
-      <div :class="['grid-stack grid-stack-' + 1]"></div>
+      <div :class="['grid-stack bar grid-stack-' + 1]"></div>
       <input type="button" value="Submit" style="color: white; text-decoration: none;" class="big-button bg-blue" @click="submitBoard">
     </div>
   </div>
@@ -24,11 +24,13 @@ export default {
     name: 'DesignBoard',
     data: function () {
         return {
-            gridWidth: 12,
-            gridHeight: 15,
+            gridWidth: null,
+            gridHeight: null,
             authCode: sessionStorage.getItem('authcode'),
             gameName: sessionStorage.getItem('gamename'),
-            playerName: sessionStorage.getItem('playername')
+            playerName: sessionStorage.getItem('playername'),
+            grid0: null,
+            grid1: null,
         }
     },
     async mounted () {
@@ -44,9 +46,16 @@ export default {
             alert("Game not found.");
             return;
         }
-
-        console.log(response.data)
         var items = response.data;
+
+        response = await Axios().post('getGridDim',
+            {
+                gameName: this.gameName,
+                playerName: this.playerName
+            }
+        );
+        this.gridWidth = response.data["x"]
+        this.gridHeight = response.data["y"]
 
         //this is pre placed to stop the grid from disappearing
         var MANDATORYitems = [
@@ -55,30 +64,37 @@ export default {
         
 
         //load grids
-        this.grids = GridStack.initAll({
+
+        this.grid0 = GridStack.init({
           dragIn: '.grid-stack-item',
           dragInOptions: { revert: 'invalid', scroll: false, appendTo: 'body', helper: 'clone' },
-          acceptWidgets: function(el) { return true; }
-        });
-        this.grids[0].float(true);
-        this.grids[0].column(this.gridWidth);
-        this.grids[0].opts.minRow = this.gridHeight;
-        this.grids[0].opts.maxRow = this.gridHeight;
-        this.grids[1].float(false);
-        this.grids[1].column(1);
-        this.grids[1].opts.cellHeight = 40; //pixels
-        this.grids[0].load(MANDATORYitems);
-        this.grids[1].load(items);
+          acceptWidgets: function(el) { return true;},
+          float: true,
+          column: this.gridWidth,
+          row: this.gridHeight,
+        }, '.grid-stack.board');
+
+        this.grid1 = GridStack.init({
+          dragIn: '.grid-stack-item',
+          dragInOptions: { revert: 'invalid', scroll: false, appendTo: 'body', helper: 'clone' },
+          acceptWidgets: function(el) { return true},
+          float: false,
+          column: 1,
+          cellHeight: 40,
+        }, '.grid-stack.bar');
+
+        this.grid0.load(MANDATORYitems);
+        this.grid1.load(items);
     },
     methods: {
       async submitBoard(){
-        serializedData = grid.save();
+        var serializedData = this.grid0.save();
         let response = null;
         response = await Axios().post('saveBoard',
             {
                 gameName: this.gameName,
                 playerName: this.playerName,
-                authCode: this.authcode,
+                authCode: this.authCode,
                 board: serializedData
             });
         if (response.data["game"] == false){
@@ -87,17 +103,17 @@ export default {
 
       },
       async randomiseBoard (){
-        this.grids[1].removeAll();
+        this.grid1.removeAll();
         let response = null;
         response = await Axios().post('randomiseBoard',
               {
                   gameName: this.gameName,
                   playerName: this.playerName,
-                  authCode: this.authcode
+                  authCode: this.authCode
               });
         var board = response.data;
         console.log(board);
-        this.grids[0].load(board, true);
+        this.grid0.load(board, true);
       }
     }
 }
