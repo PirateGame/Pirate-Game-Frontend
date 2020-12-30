7d8f791b8617f28f0ba77c6f7138023d116738d7
portaljumper007 <enchantedportal007@gmail.com>
2020-12-30 19:24:32 +0000

    more fancy styling

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="bg-pirate">
-        <h1 class="title">The Pirate Game</h1>
+        <h1 class="title3">The</h1>
+        <h1 class="title1">Pirate</h1>
+        <h1 class="title2">Game</h1>
         <div class="mainMenu">
             <ul id="menuUL">
                 <li class="bg-joinColour">
-                    <router-link :to="{ name: 'JoinPage' }">Join Game</router-link>
+                    <router-link :to="{ name: 'JoinPage' }">Join</router-link>
                 </li>
                 <li class="bg-hostColour">
-                    <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
+                    <router-link :to="{ name: 'HostPage' }">Host</router-link>
                 </li>
                 <li id="inputLI">
                 </li>
             </ul>
+        <h1 class="bottomText1">Developed by Alex.P and Jamie.E</h1>
         </div>
     </div>
 </template>
