fff81ef65d368e613bd32c174e1971a072fd3eb8
portaljumper007 <enchantedportal007@gmail.com>
2020-12-30 19:11:35 +0000

    new background and styles

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -3,10 +3,10 @@
         <h1 class="title">The Pirate Game</h1>
         <div class="mainMenu">
             <ul id="menuUL">
-                <li class="bg-green">
+                <li class="bg-joinColour">
                     <router-link :to="{ name: 'JoinPage' }">Join Game</router-link>
                 </li>
-                <li class="bg-blue">
+                <li class="bg-hostColour">
                     <router-link :to="{ name: 'HostPage' }">Host Game</router-link>
                 </li>
                 <li id="inputLI">
