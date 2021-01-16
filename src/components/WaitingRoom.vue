ecac6acf659d621b575bb91fdd81b546226f0048
Alex40144 <giddyraccoon@gmail.com>
2021-01-16 18:02:06 +0000

    re organised starting games

--- src/components/WaitingRoom.vue
+++ src/components/WaitingRoom.vue
@@ -4,6 +4,14 @@
         <h2>Please wait while the host starts the game.</h2>
         <h2>Here is a guide on how to play the game.</h2>
         <h2> game state: {{gameState}} </h2>
+        <div v-show="isHost">
+            <div v-show="!isReady" class="flex-child" style="margin-top:0">
+                <input type="button" value="Play" style="color: white; text-decoration: none;" class="big-button bg-red">
+            </div>
+            <div v-show="isReady" class="flex-child" style="margin-top:0">
+                <input type="button" value="Play" style="color: white; text-decoration: none;" class="big-button bg-green" @click="startGame">
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,35 +21,75 @@ export default {
     name: 'WaitingRoom',
     data: function () {
         return {
-            secretcode: sessionStorage.getItem('authcode'),
+            authCode: sessionStorage.getItem('authcode'),
             gameName: sessionStorage.getItem('gamename'),
+            playerName: sessionStorage.getItem("playername"),
             gameState: "unknown",
             timerID: null,
+            isHost: false,
+            isReady: false,
         }
     },
     async created () {
         this.timerID = setInterval(this.gameCheck, 5000);
+        this.amIhost()
+        this.gameCheck()
     },
     methods: {
         async gameCheck(){
             let response = null;
-            response = await Axios().post('getGameState',
+            response = await Axios().post('lobbyCheck',
                 {
-                    gameName: this.gameName
+                    gameName: this.gameName,
+                    playerName: this.playerName,
+                    authCode: this.authCode,
                 });
             if (response.data["error"] != false){
-                console.log("error:")
                 console.log(response.data["error"])
             }
-            else {
-                this.gameState = response.data["state"]
+            this.gameState = response.data["state"]
+            console.log(this.gameState)
+            if (this.gameState == "ready") {
+                this.isReady = true
             }
-            console.log(response.data)
-            if (this.gameState == "building"){
-                clearInterval(this.timerID)
-                router.push("/PickTeam")
+            if (this.gameState == "started") {
+                router.push("/Game")
             }
-        }
+        },
+        async amIhost(){
+            var response = null;
+            response = await Axios().post('amIHost',
+                {
+                    gameName: this.gameName,
+                    playerName: this.playerName,
+                    authCode: this.authCode,
+                }
+            );
+            if (response.data["error"] != false){
+                console.log(response.data["error"])
+                return;
+            }
+            else{
+                this.isHost = true;
+            }
+        },
+        async startGame(){
+                clearInterval(this.gameStateTimer)
+                var response = null;
+                response = await Axios().post('startGame',
+                    {
+                        gameName: this.gameName,
+                        playerName: this.playerName,
+                        authCode: this.authCode,
+                    }
+                );
+                if (response.data["error"] != false){
+                    console.log(response.data["error"]);
+                }
+                else {
+                    this.gameCheck()
+                }
+            },
 
     }
 }
