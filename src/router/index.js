a2e1c53e3bb441fef643cbb54f601f33ac87e5f9
Alex40144 <giddyraccoon@gmail.com>
2021-01-17 10:43:50 +0000

    looking promising

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '../../src/components/landingPage.vue'
 import HostPage from '../../src/components/HostPage.vue'
 import JoinPage from '../../src/components/JoinPage.vue'
-import WaitingRoom from 'src/components/WaitingRoom.vue'
-import HostPanel from 'src/components/HostPanel.vue'
-import AdminPanel from 'src/components/AdminPanel.vue'
-import DesignBoard from 'src/components/DesignBoard.vue'
-import PickTeam from 'src/components/PickTeam.vue'
-import Game from 'src/components/Game.vue'
-import Rules from 'src/components/Rules.vue'
+import WaitingRoom from '../../src/components/WaitingRoom.vue'
+import HostPanel from '../../src/components/HostPanel.vue'
+import AdminPanel from '../../src/components/AdminPanel.vue'
+import DesignBoard from '../../src/components/DesignBoard.vue'
+import PickTeam from '../../src/components/PickTeam.vue'
+import Game from '../../src/components/Game.vue'
+import Rules from '../../src/components/Rules.vue'
 
 let router = createRouter({
     history: createWebHistory(),
