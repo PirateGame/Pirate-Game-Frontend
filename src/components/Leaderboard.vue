a6309c66eec7604627aebd49359dbbfa87d7e637
portaljumper007 <enchantedportal007@gmail.com>
2021-01-17 19:55:29 +0000

    The beginning of the leaderboard page.

---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-leaderboardpage">
+        <br><br><br><br><br><br><br>
+        <h1 class="title1">Leaderboard</h1>
+        <br><br><br><br><br>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Rules'
+}
+</script>
