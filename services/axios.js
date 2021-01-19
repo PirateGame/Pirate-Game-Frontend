24e6b308e7a2ab559602a28a165dec045ba2bb9c
Alex40144 <giddyraccoon@gmail.com>
2021-01-19 17:53:33 +0000

    added lines for localhost and server

--- services/axios.js
+++ services/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: 'http://pirategame.uk/api/',
+        baseURL: 'http://localhost:5000/api/',
+        //baseURL: 'http://pirategame.uk/api/',
         withCredentials: true,
         headers: {
             Accept: 'application/json',
