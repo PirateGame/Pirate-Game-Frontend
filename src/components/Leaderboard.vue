447a7794db89ee9bb539aea90d11767fe0b49328
Alex Pegg <alexpeggsailing@gmail.com>
2021-03-10 21:09:13 +0000

    leaderboard page

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,13 +1,69 @@
 <template>
     <div class="bg-leaderboardpage">
-        <br><br><br><br><br><br><br>
-        <h1 class="title1">Leaderboard</h1>
-        <br><br><br><br><br>
+        <div class="leaderboard">
+            <canvas id="myChart"></canvas>
+        </div>
     </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
 export default {
-    name: 'Rules'
+    name: 'Leaderboard',
+    async mounted() {
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['1', '2', '3', '4', '5', '6'],
+                datasets: [{
+                    label: 'Alex',
+                    data: [12, 19, 240, 5, 2, 3],
+                    backgroundColor: ['rgb(255, 0, 0)'],
+                    borderColor: ['rgb(255, 0, 0)'],
+                    fill: false,
+                },
+                {
+                    label: 'Jamie',
+                    data: [23, 12, 212, 34, 12, 54],
+                    backgroundColor: ['rgb(0, 255, 0)'],
+                    borderColor: ['rgb(0, 255, 0)'],
+                    fill: false,
+                }]
+            },
+            options: {
+                responsive: true,
+				title: {
+					display: true,
+					text: 'Leaderboard'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Turn'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Gold Coins'
+						}
+					}]
+				}
+			}
+        });
+    }
+
 }
 </script>
